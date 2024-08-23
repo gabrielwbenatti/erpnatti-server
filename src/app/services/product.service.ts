b63e5ab7127fab58527d7ffe52bdc7814e09a02e
Gabriel Benatti <gabriel@infonacci.com.br>
2024-08-23 12:55:46 -0300

    add nodemon e ajuste de nulidade

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -14,7 +14,7 @@ class ProductsService {
         movimenta_estoque: body.movimenta_estoque || true,
         estoque_minimo: body.estoque_minimo || 0.0,
         estoque_maximo: body.estoque_maximo || 0.0,
-        grupo_produto_id: body.grupo_produto_id || null,
+        grupo_produto_id: body.grupo_produto?.id || null,
       },
     });
 
