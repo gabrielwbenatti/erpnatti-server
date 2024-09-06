a39f0b9a5b3e5fff3923536a97218d65be0692b6
Gabriel Benatti <gabriel@infonacci.com.br>
2024-09-06 17:24:32 -0300

    alterado PORT

--- src/app/index.ts
+++ src/app/index.ts
@@ -5,7 +5,7 @@ import router from "./routes/v1";
 
 async function main() {
   const app = express();
-  const port = 3000;
+  const port = 3001;
 
   app.use(express.json());
   app.use(cors<Request>());
